413ed012d9020b42173e74f99ba6d320962045c8
Sanjna Dadral <your-email@gmail.com>
2026-04-12 19:54:19 +0530

    Enable debug to find error

--- paper_analyzer/settings.py
+++ paper_analyzer/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dev-key-change-in-production-12345')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["research-8.onrender.com"]
 
